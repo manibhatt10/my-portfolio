a36390ca324645f6f3c8847b7bb93def8a1f91eb
Manorath Bhatt <bhattmani10@gmail.com>
2026-03-17 00:33:46 +0545

    smoothly animate dark mode toggle

--- script.js
+++ script.js
@@ -45,22 +45,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // --- Theme Toggle ---
-    const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-    const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
     const themeBtn = document.getElementById('theme-toggle');
 
-    if (themeToggleDarkIcon && themeToggleLightIcon && themeBtn) {
-        // Change the icons inside the button based on previous settings
-        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            themeToggleLightIcon.classList.remove('hidden');
-        } else {
-            themeToggleDarkIcon.classList.remove('hidden');
-        }
-
+    if (themeBtn) {
         themeBtn.addEventListener('click', function () {
-            // toggle icons inside button
-            themeToggleDarkIcon.classList.toggle('hidden');
-            themeToggleLightIcon.classList.toggle('hidden');
+            // CSS handles rotating icons automatically via the 'dark' top-level class.
 
             // if set via local storage previously
             if (localStorage.getItem('color-theme')) {
